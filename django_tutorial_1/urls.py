1f91133725127652f35e4ceeb146d0de3507daaa
Deniss Matvejevs <mden7785@gmail.com>
2018-07-12 16:55:44 +0000

    Add all

---
+++ django_tutorial_1/urls.py
@@ -0,0 +1,27 @@
+"""django_tutorial_1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from todo.views import open_to_do, create_an_item, edit_an_item, toggle_status
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', open_to_do),
+    path('add', create_an_item),
+    url(r'^edit/(?P<id>\d+)$', edit_an_item),
+    url(r'^toggle/(?P<id>\d+)$', toggle_status),
+]
